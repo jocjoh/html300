e02efe52e32be6208f10f59c857e6672ef008100
Joce Johnson <51525909+jocjoh@users.noreply.github.com>
2020-01-28 22:08:55 -0800

    Loop through array creating modules for all data objects

--- lesson03/assignment/index.js
+++ lesson03/assignment/index.js
@@ -1,37 +1,32 @@
-const person = {
-    name: "Steve Smith",
-    jobTitle: "Project Manager",
-    company: "Front End Dev Co",
-    experience: "3 years",
-    school: "UW",
-    major: "Marketing",
-    email: "steve@fedc.com",
-    linkedInUrl: "steve.linkedinprofile.com",
-    codeLanguages: [
-        "HTML", "CSS", "JavaScript", ".NET", "C#"
-    ]
-  }
+const guys = [
+    { name: "Steve Smith", jobTitle: "Project Manager", company: "Front End Dev Co", experience: "3 years", school: "UW", major: "Marketing", email: "steve@fedc.com", linkedInUrl: "steve.linkedinprofile.com" },
+    { name: "Aaron Katz", jobTitle: "Full Stack Developer", company: "Web Sites and More", experience: "5 years", school: "SU", major: "Computer Science", email: "aaronNotMyemail@uw.com", linkedInUrl: "aaron.linkedinprofile.com"},
+    { name: "Kyle Hendricks", jobTitle: "Starting Pitcher", company: "Chicago Cubs", experience: "12 years", school: "USC", major: "Pitching", email: "kyleH@cubs.com", linkedInUrl: "kyle.linkedinprofile.com"},
+    { name: "Michael Jordan", jobTitle: "Point Guard", company: "Chicago Bulls", experience: "20 years", school: "UNC", major: "Trash Talking", email: "mJordan@bulls.com", linkedInUrl: "mJordan.linkedinprofile.com"},
+];
 
 const myCode = `
+    ${guys.map(guys => `
     <main class="main">
       <div class="background">
         <div class="photo-title">
           <img src="img/unsplash-headshot.jpg" class="photo-title__headshot" alt="Headshot for Steve Smith in blue shirt">
-          <h1 class="photo-title__name">${person.name}</h1>
-          <h2 class="photo-title__title">${person.jobTitle}</h2>
+          <h1 class="photo-title__name">${guys.name}</h1>
+          <h2 class="photo-title__title">${guys.jobTitle}</h2>
         </div>
       <div class="info">
         <div class="profile">
-          <p class="profile__para"><strong>Company:</strong> ${person.company}</p>
-          <p class="profile__para"><strong>Experience:</strong> ${person.experience}</p>
-          <p class="profile__para"><strong>School:</strong> ${person.school}</p>
-          <p class="profile__para"><strong>Major:</strong> ${person.major}</p>
-          <p class="profile__para"><strong>Email:</strong> ${person.email}</p>
-          <p class="profile__para"><img src="img/linkedin.svg" class="linkedin" alt="LinkedIn logo"> ${person.linkedInUrl}</p>
-          <p class="profile__para"><strong>Code languages:</strong> ${person.codeLanguages}</p>
+          <p class="profile__para"><strong>Company:</strong> ${guys.company}</p>
+          <p class="profile__para"><strong>Experience:</strong> ${guys.experience}</p>
+          <p class="profile__para"><strong>School:</strong> ${guys.school}</p>
+          <p class="profile__para"><strong>Major:</strong> ${guys.major}</p>
+          <p class="profile__para"><strong>Email:</strong> ${guys.email}</p>
+          <p class="profile__para"><img src="img/linkedin.svg" class="linkedin" alt="LinkedIn logo"> ${guys.linkedInUrl}</p>
+          <p class="profile__para"><strong>Code languages:</strong> ${guys.codeLanguages}</p>
         </div>
       </div>
-    </main>`;
+    </main>`
+    )}
+`;
 
-
-document.body.innerHTML = myCode;
+ document.body.innerHTML = myCode;
